546db7d13ac15c3025fbb77f8a57a5467cc70d6b
KaoKsn <mayurakrishna.25@proton.me>
2026-08-03 14:30:09 +0530

    add app/: src for the fastapi service

---
+++ app/main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI, Response
+from fastapi.responses import RedirectResponse, JSONResponse
+from starlette.status import HTTP_404_NOT_FOUND
+from dotenv import load_dotenv
+import random
+from cs50 import SQL
+import os
+
+load_dotenv()
+
+app = FastAPI(
+    description = "API(FastAPI) endpoints with k8s deployment",
+    version = "v1.0"
+)
+
+db = SQL(os.getenv("DB_URL"))
+
+""" Utility Endpoints """
+@app.get("/")
+async def index():
+    return RedirectResponse(url="/health")
+
+@app.get("/health")
+async def health():
+    return {
+        "status": "healthy",
+        "code": 200,
+        "time": db.execute("SELECT datetime() AS now")[0]['now'],
+        "version": os.getenv("API_VERSION")
+    }
+
+@app.get("/info")
+async def randominfo():
+    rows = db.execute("SELECT name FROM student")
+    names = [r['name'] for r in rows]
+    return {
+        "rand_str": os.getenv("USER_NAME"),
+        "rand_pstr": os.getenv("USER_PASSWORD"),
+        "names": names
+    }
+
+@app.get("/random/{guess}")
+async def search(guess: int):
+    if guess == random.randint(1, 10):
+        return {
+            "message": "Great, the same pseudo-random number was generated"
+        }
+    else:
+        return {
+            "message": "Invalid random number guessed, try something between (1, 10)"
+        }
+
+""" Error Handlers """
+@app.exception_handler(404)
+async def not_found_handler(request: Response, exc):
+    return JSONResponse(
+        status_code=HTTP_404_NOT_FOUND,
+        content={
+            "message": "Not found",
+            "path": str(request.url)
+        }
+    )
+
+@app.exception_handler(500)
+async def internal_server_error(request: Response, exc):
+    return JSONResponse(
+        status_code=500,
+        content = {
+            "message": "Internal Sever Error.",
+            "path": str(request.url)
+        }
+    )
